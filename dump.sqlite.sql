1a820c0de003e00bb81e4bc6010d4fc12097a10b
Akshara Gopakumar <71522990+Aksh-99@users.noreply.github.com>
2023-10-30 18:42:03 +0000

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,11 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE "netflix" ('show_id' TEXT,'type' TEXT,'title' TEXT,'director' TEXT,"movie_cast" TEXT,'date_added' TEXT,'release_year' INTEGER,'rating' TEXT,'duration' TEXT,'listed_in' TEXT,'description' TEXT);
+CREATE TABLE "netflix00" ('show_id' TEXT,'type' TEXT,'title' TEXT,'director' TEXT,'country' TEXT,'date_added' TEXT,'release_year' INTEGER,'rating' TEXT,'duration' TEXT,'listed_in' TEXT);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
